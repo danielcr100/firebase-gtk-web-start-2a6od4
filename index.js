d4703dca4aa06eb7a3e9fc282d533da1da76fa3b
danielcr100 <danielcr100@gmail.com>
2020-04-23 20:55:14 -0600

    2 files modified

--- index.js
+++ index.js
@@ -67,10 +67,12 @@ if (user) {
 startRsvpButton.textContent = "LOGOUT";
 guestbookContainer.style.display = "block"
 subscribeGuestbook()
+subscribeCurrentRSVP(user)
 } else {
 startRsvpButton.textContent = "RSVP";
 guestbookContainer.style.display = "none"
 unsubscribeGuestbook()
+subscribeCurrentRSVP()
 }
 });
 
@@ -122,8 +124,34 @@ rsvpNo.onclick =() => {
   }).catch(console.error);
 }
 
-firebase.firestore().collection("attendees")
-.where("attending","==", true).onSnapshot((snap) =>{
-  const newAttendeeCount = snap.docs.length;
-  numberAttending.innetHTML = newAttendeeCount + 'people going';
-});
+
+
+firebase.firestore().collection('attendees').where("attending", "==", true).onSnapshot(snap => { const newAttendeeCount = snap.docs.length; numberAttending.innerHTML = newAttendeeCount+' people going'; 
+})
+
+function subscribeCurrentRSVP (user){
+  rsvpListener = firebase.firestore().collection('attendees')
+  .doc(user.uid).onSnapshot((doc)=>{
+    if (doc&& doc.data()){
+      const attendingResponse = doc.data().attending;
+
+      if (attendingResponse){
+        rsvpYes.className="clicked";
+        rsvpNo.className="";
+       } else {
+        rsvpYes.className="";
+        rsvpNo.className="clicked";
+        
+      }
+    }
+  })
+}
+
+function unsubscribeCurrentRSVP(){
+  if(rsvpListener !=null){
+    rsvpListener();
+    rsvpListener=null;
+  }
+  rsvpYes.className="";
+  rsvpNo.className="";
+}
